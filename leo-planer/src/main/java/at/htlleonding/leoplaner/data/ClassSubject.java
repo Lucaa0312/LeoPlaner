c877c4a4aec257980582ac7125bd0e5b669dcfb2
AndiMisic <a.misic@students.htl-leonding.ac.at>
2025-11-04 13:51:58 +0100

    klassendiagramm ausprogrammiert

--- leo-planer/src/main/java/at/htlleonding/leoplaner/data/ClassSubject.java
+++ leo-planer/src/main/java/at/htlleonding/leoplaner/data/ClassSubject.java
@@ -2,15 +2,13 @@ package at.htlleonding.leoplaner.data;
 
 public class ClassSubject {
     private Teacher teacher;
-    private Room room;
     private Subject subject;
     private short weeklyHours;
     private boolean requiresDoublePeriod;
     private boolean isBetterDoublePeriod;
 
-    public ClassSubject(Teacher teacher, Room room, Subject subject, short weeklyHours, boolean requiresDoublePeriod, boolean isBetterDoublePeriod) {
+    public ClassSubject(Teacher teacher, Subject subject, short weeklyHours, boolean requiresDoublePeriod, boolean isBetterDoublePeriod) {
         this.teacher = teacher;
-        this.room = room;
         this.subject = subject;
         this.weeklyHours = weeklyHours;
         this.requiresDoublePeriod = requiresDoublePeriod;
@@ -19,10 +17,6 @@ public class ClassSubject {
 
     public Teacher getTeacher() {
         return teacher;
-    }
-
-    public Room getRoom() {
-        return room;
     }
 
     public Subject getSubject() {
