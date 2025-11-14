f6c30dfd20554c264b698a7fd8ec8e8b936aa102
Sean-Cinac <s.cinac@students.htl-leonding.ac.at>
2025-11-14 08:04:03 +0100

    csvParser progress

--- leo-planer/src/main/java/at/htlleonding/leoplaner/data/CSVManager.java
+++ leo-planer/src/main/java/at/htlleonding/leoplaner/data/CSVManager.java
@@ -7,6 +7,8 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class CSVManager {
+  // TODO decide make static or non static
+  private CSVManager csvManager;
   final static String teacherType = "teacher";
   final static String roomType = "room";
   final static String subjectType = "subject";
@@ -22,10 +24,13 @@ public class CSVManager {
 
     switch (lines[1].split(";")[0]) {
       case teacherType:
+        createTeacherFromCSV(lines);
         break;
       case roomType:
+        createRoomFromCSV(lines);
         break;
       case subjectType:
+        createSubjectFromCSV(lines);
         break;
     }
 
@@ -40,6 +45,27 @@ public class CSVManager {
       String nameSymbol = line[1];
       // TODO taken subjects split and generate array + create
       // TODO handle DataRepository not being static
+    }
+  }
+
+  public static void createRoomFromCSV(String[] lines) {
+    for (int i = 1; i < lines.length; i++) {
+      String[] line = lines[i].split(";");
+
+      String roomName = line[0];
+      String roomPrefix = line[1];
+      String roomSuffix = line[2];
+      // TODO handle roomtypes array being parsed correctly
+      DataRepository.
+    }
+  }
+
+  public static void createSubjectFromCSV(String[] lines) {
+    for (int i = 1; i < lines.length; i++) {
+      String[] line = lines[i].split(";");
+
+      String subjectName = line[0];
+      // TODO handle required roomtypes array being parsed correctly
     }
   }
 
--- leo-planer/src/main/java/at/htlleonding/leoplaner/data/DataRepository.java
+++ leo-planer/src/main/java/at/htlleonding/leoplaner/data/DataRepository.java
@@ -3,33 +3,40 @@ package at.htlleonding.leoplaner.data;
 import java.util.ArrayList;
 
 public class DataRepository {
-    private ArrayList<ClassSubject> classSubjects;
-    private ArrayList<Teacher> teachers;
-    private ArrayList<Room> rooms;
-    private static DataRepository instance;
+  private ArrayList<ClassSubject> classSubjects;
+  private ArrayList<Teacher> teachers;
+  private ArrayList<Room> rooms;
+  private static DataRepository instance;
 
-    public DataRepository getInstance() {
-         return instance;
-    }
-    private DataRepository() {
+  public static DataRepository getInstance() {
+    return instance;
+  }
 
-    }
-    public ArrayList<ClassSubject> getClassSubjects() {
-        return null;
-    }
-    public ArrayList<Teacher> getTeachers() {
-        return null;
-    }
-    public ArrayList<Room> getRooms() {
-        return null;
-    }
-    public boolean addTeacher(Teacher teacher) {
-        return true;
-    }
-    public boolean addRoom(Room room) {
-        return true;
-    }
-    public boolean addClassSubject(ClassSubject classSubject) {
-        return true;
-    }
+  private DataRepository() {
+
+  }
+
+  public ArrayList<ClassSubject> getClassSubjects() {
+    return null;
+  }
+
+  public ArrayList<Teacher> getTeachers() {
+    return null;
+  }
+
+  public ArrayList<Room> getRooms() {
+    return null;
+  }
+
+  public boolean addTeacher(Teacher teacher) {
+    return true;
+  }
+
+  public boolean addRoom(Room room) {
+    return true;
+  }
+
+  public boolean addClassSubject(ClassSubject classSubject) {
+    return true;
+  }
 }
