d08841ec628cbfccdbc5b25fd312036892008806
Sean-Cinac <s.cinac@students.htl-leonding.ac.at>
2025-11-17 14:16:23 +0100

    added test csv files and enhancements

--- leo-planer/src/main/java/at/htlleonding/leoplaner/data/CSVManager.java
+++ leo-planer/src/main/java/at/htlleonding/leoplaner/data/CSVManager.java
@@ -11,6 +11,8 @@ public class CSVManager {
   final static String teacherType = "teacher";
   final static String roomType = "room";
   final static String subjectType = "subject";
+  // TODO final static String testPath =
+  // "src/resources/csvFiles/test1/testTeacher.csv";
 
   public static boolean processCSV(String filePath, DataRepository dataRepository) {
     String[] lines = getLinesFromCSV(filePath);
@@ -19,7 +21,7 @@ public class CSVManager {
       return false;
     }
 
-    switch (lines[0].split(";")[0]) {
+    switch (lines[0].split(";")[0].toLowerCase()) {
       case teacherType:
         createTeacherFromCSV(lines, dataRepository);
         break;
@@ -36,22 +38,22 @@ public class CSVManager {
 
   public static void createTeacherFromCSV(String[] lines, DataRepository dataRepository) {
     for (int i = 1; i < lines.length; i++) {
-      String[] line = lines[i].split(";");
+      String[] line = lines[i].toLowerCase().split(";");
 
       // FULL CSV FORMAT EXAMPLE: ;John Doe;JD;math,physics,chemistry.CHEM,HISTORY;
       // (.CHEM is roomtype)
 
-      String teacherName = line[0];
-      String nameSymbol = line[1];
+      String teacherName = line[0].trim();
+      String nameSymbol = line[1].trim();
 
       ArrayList<Subject> takenSubjects = new ArrayList<>(); // CSV FORMAT: ;math,physics(maybe roomtypes with ..);
       String[] subjects = line[2].split(",");
       for (String subjectName : subjects) {
-        Subject subject = dataRepository.checkIfSubjectExists(subjectName);
+        Subject subject = dataRepository.checkIfSubjectExists(subjectName.trim());
 
         if (subject == null) { // MAYBE fix this with an error message "Subject not found please import subject
                                // csv first"
-          subject = new Subject(subjectName, null); // TODO add roomtypes in CSV
+          subject = new Subject(subjectName.trim(), null); // TODO add roomtypes in CSV
           dataRepository.addSubject(subject);
         }
         takenSubjects.add(subject);
@@ -64,23 +66,23 @@ public class CSVManager {
 
   public static void createRoomFromCSV(String[] lines, DataRepository dataRepository) {
     for (int i = 1; i < lines.length; i++) {
-      String[] line = lines[i].split(";");
+      String[] line = lines[i].toLowerCase().split(";");
 
       // FULL CSV FORMAT EXAMPLE: 101;EDUARD;;;CHEM,PHY;
 
       short roomNumber = Short.parseShort(line[0]);
-      String roomName = line[1];
-      String roomPrefix = line[2];
-      String roomSuffix = line[3];
+      String roomName = line[1].trim();
+      String roomPrefix = line[2].trim();
+      String roomSuffix = line[3].trim();
 
-      String[] roomTypesStrings = line[4].split(",");
+      String[] roomTypesStrings = line[4].split(","); // TODO handle empty prefix/suffix
       RoomTypes[] roomTypesArray = new RoomTypes[roomTypesStrings.length];
 
       int index = 0;
       for (String roomTypeString : roomTypesStrings) {
         RoomTypes roomType;
         try {
-          roomType = RoomTypes.valueOf(roomTypeString);
+          roomType = RoomTypes.valueOf(roomTypeString.trim().toUpperCase());
           roomTypesArray[index] = roomType;
           index++;
         } catch (IllegalArgumentException e) {
@@ -95,10 +97,10 @@ public class CSVManager {
 
   public static void createSubjectFromCSV(String[] lines, DataRepository dataRepository) {
     for (int i = 1; i < lines.length; i++) {
-      String[] line = lines[i].split(";");
+      String[] line = lines[i].toLowerCase().split(";");
 
       // FULL CSV LINE FORMAT EXAMPLE: Chemisty;CHEM,PHY;
-      String subjectName = line[0];
+      String subjectName = line[0].trim();
       String[] requiredRoomTypesStrings = line[1].split(",");
       RoomTypes[] requiredRoomTypes = new RoomTypes[requiredRoomTypesStrings.length];
 
@@ -106,7 +108,7 @@ public class CSVManager {
       for (String roomTypeString : requiredRoomTypesStrings) {
         RoomTypes roomType;
         try {
-          roomType = RoomTypes.valueOf(roomTypeString);
+          roomType = RoomTypes.valueOf(roomTypeString.trim().toUpperCase());
           requiredRoomTypes[index] = roomType;
           index++;
         } catch (IllegalArgumentException e) {
