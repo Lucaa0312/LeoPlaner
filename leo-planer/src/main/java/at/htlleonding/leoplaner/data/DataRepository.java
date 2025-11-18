589f2669f5efe82078dd65989ada088944924cf0
Sean-Cinac <s.cinac@students.htl-leonding.ac.at>
2025-11-18 21:49:06 +0100

    added Resource api file, getAllClassSubjects method

--- leo-planer/src/main/java/at/htlleonding/leoplaner/data/DataRepository.java
+++ leo-planer/src/main/java/at/htlleonding/leoplaner/data/DataRepository.java
@@ -1,35 +1,41 @@
 package at.htlleonding.leoplaner.data;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class DataRepository {
-    private ArrayList<ClassSubject> classSubjects;
-    private ArrayList<Teacher> teachers;
-    private ArrayList<Room> rooms;
-    private static DataRepository instance;
+  private ArrayList<ClassSubject> classSubjects;
+  private ArrayList<Teacher> teachers;
+  private ArrayList<Room> rooms;
 
-    public DataRepository getInstance() {
-         return instance;
-    }
-    private DataRepository() {
+  public List<ClassSubject> getAllClassSubjects() { // Extend with TypedQuery once DataBase is set up
+    return classSubjects;
+  }
 
-    }
-    public ArrayList<ClassSubject> getClassSubjects() {
-        return null;
-    }
-    public ArrayList<Teacher> getTeachers() {
-        return null;
-    }
-    public ArrayList<Room> getRooms() {
-        return null;
-    }
-    public boolean addTeacher(Teacher teacher) {
-        return true;
-    }
-    public boolean addRoom(Room room) {
-        return true;
-    }
-    public boolean addClassSubject(ClassSubject classSubject) {
-        return true;
-    }
+  public ArrayList<ClassSubject> getClassSubjects() {
+    return null;
+  }
+
+  public ArrayList<Teacher> getTeachers() {
+    return null;
+  }
+
+  public ArrayList<Room> getRooms() {
+    return null;
+  }
+
+  public boolean addTeacher(Teacher teacher) {
+    return true;
+  }
+
+  public boolean addRoom(Room room) {
+    return true;
+  }
+
+  public boolean addClassSubject(ClassSubject classSubject) {
+    return true;
+  }
 }
