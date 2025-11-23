b7df7e2435f746fb903382f10760d2e08dd37edf
Marc Preslmair <m.preslmair@htblaleonding.onmicrosoft.com>
2025-11-23 20:41:25 +0100

    removed getAllSubjects  method

--- leo-planer/src/main/java/at/htlleonding/leoplaner/boundary/Resource.java
+++ leo-planer/src/main/java/at/htlleonding/leoplaner/boundary/Resource.java
@@ -2,15 +2,15 @@ package at.htlleonding.leoplaner.boundary;
 
 import java.util.List;
 
-import at.htlleonding.leoplaner.data.*;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
+import at.htlleonding.leoplaner.data.CSVManager;
+import at.htlleonding.leoplaner.data.DataRepository;
+import at.htlleonding.leoplaner.data.Subject;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 
