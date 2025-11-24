135aff359e2071a0f2b7a3aba848a2ef707d0607
Marc Preslmair <m.preslmair@htblaleonding.onmicrosoft.com>
2025-11-24 20:39:41 +0100

    git javascript in web branch added

--- Web/javascript/script.js
+++ Web/javascript/script.js
@@ -1,45 +1,50 @@
+let times = [
+    "07:05", "07:55", "08:00", "08:50", "08:55",
+    "09:45", "10:00", "10:50", "10:55", "11:45",
+    "11:50", "12:40", "12:45", "13:35", "13:40",
+    "14:30", "14:35", "15:25", "15:30", "16:20",
+    "16:25", "17:15", "17:20", "18:05", "18:50",
+    "19:00", "19:45", "20:30", "20:40", "21:25",
+    "22:10"
+];
+
+const daysOfWeek = [
+    "Time schedules",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday"
+];
+
+let currentDay = "";
+
 function load() {
-    let times = [
-        "07:05", 
-        "07:55", 
-        "08:00",
-        "08:50", 
-        "08:55",
-        "09:45", 
-        "10:00",
-        "10:50", 
-        "10:55",
-        "11:45", 
-        "11:50",
-        "12:40", 
-        "12:45",
-        "13:35", 
-        "13:40",
-        "14:30", 
-        "14:35",
-        "15:25",
-        "15:30",
-        "16:20", 
-        "16:25",
-        "17:15", 
-        "17:20",
-        "18:05",
-        "18:50", 
-        "19:00",
-        "19:45",
-        "20:30", 
-        "20:40",
-        "21:25",
-        "22:10"
-    ];
+    fetch("http://localhost:8080/api/timetable")
+    .then(response => {
+        return response.json();
+    }).then(data => {
+        data = data.classSubjectInstances;
+        console.log(data);
 
-    let builder = "";
-    let builderDays = "";
+        let builder = {
+            "0": "", // "Time schedules"
+            "1": "", // Monday
+            "2": "", // Tuesday
+            "3": "", // Wednesday
+            "4": "", // Thursday
+            "5": "",  // Friday
+            "6": "" // Saturday 
+        };
 
-    for (let i = 0; i < times.length; i+=2) {
-        builderDays += `<div class="periodStyling"><p class="period">Lesson ${i}</p></div>`;
-        builder += `<div id="hour${i}"><p class="periodStarted"> ${times[i]}</p> <p class="periodEnded">${times[i+1] ?? ""}</p></div>`;
-    }
+        let daySubjects = {
+            "1": new Array(times.length).fill(null),
+            "2": new Array(times.length).fill(null),
+            "3": new Array(times.length).fill(null),
+            "4": new Array(times.length).fill(null),
+            "5": new Array(times.length).fill(null),
+            "6": new Array(times.length).fill(null)
+        };
 
         function determineSlotIndex(item) {
             try {
@@ -92,7 +97,7 @@ function load() {
         });
 
         for (let i = 0; i < times.length; i+=2) {
-            builder["0"] += `<div class="timeScheduleBoxes"><p class="periodStarted">${times[i]}</p> <p class="periodEnded">${times[i + 1] || ""}</p></div>\n`;
+            builder["0"] += `<div id="hour${i}"><p class="periodStarted">${times[i]}</p> <p class="periodEnded">${times[i + 1] || ""}</p></div>\n`;
         }
 
         for (let d = 1; d <= 6; d++) {
@@ -115,11 +120,6 @@ function load() {
     }).catch(error => {
         console.error('Error fetching data:', error);
     });
-
-    document.querySelectorAll('.gridBoxDays').forEach(box => {
-        box.innerHTML = builderDays;
-    });
-
 }
 
 load();
