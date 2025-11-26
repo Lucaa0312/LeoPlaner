30b7ec813b7bd2424be9b291dea7be84e92cdf4e
Sean-Cinac <s.cinac@students.htl-leonding.ac.at>
2025-11-26 17:06:36 +0100

    new neighbor functions

--- leo-planer/src/main/java/at/htlleonding/leoplaner/algorithm/SimulatedAnnealingAlgorithm.java
+++ leo-planer/src/main/java/at/htlleonding/leoplaner/algorithm/SimulatedAnnealingAlgorithm.java
@@ -1,11 +1,14 @@
 package at.htlleonding.leoplaner.algorithm;
 
 import at.htlleonding.leoplaner.data.ClassSubjectInstance;
+import at.htlleonding.leoplaner.data.DataRepository;
 import at.htlleonding.leoplaner.data.Room;
 import at.htlleonding.leoplaner.data.Teacher;
 import at.htlleonding.leoplaner.data.Timetable;
+import jakarta.inject.Inject;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class SimulatedAnnealingAlgorithm {
     private Timetable currTimeTable;
@@ -15,6 +18,42 @@ public class SimulatedAnnealingAlgorithm {
     private final double INIT_TEMPERATURE = 1000.0;
     private final int ITERATIONS = 100000;
     private final double COOLING_RATE = 0.995;
+
+    @Inject 
+    DataRepository dataRepository;
+
+
+    public void algorithmLoop() {
+        this.currTimeTable = this.dataRepository.getCurrentTimetable();
+        Random random = new Random();
+        for (int i = 0; i < ITERATIONS; i++) {
+            int indexesAmount = this.currTimeTable.getClassSubjectInstances().size();
+            int ranIndex1 = random.nextInt(0, indexesAmount);
+            int ranIndex2;
+
+            do {
+                ranIndex2 = random.nextInt(0, indexesAmount);
+            } while (ranIndex2 == ranIndex1);
+
+            
+            
+        }
+    }
+
+    public void chooseRandomNeighborFunction(int index1, int index2) {
+        Random random = new Random();
+
+        int ranNumber = random.nextInt(1, 2);
+        
+        switch(ranNumber) {
+            case 1:
+              swapPeriods(index1, index2);
+              break;
+            case 2:
+              changePeriod(index1);
+              break;
+        }
+    }
 
     public void duplicateTimeTableIntoNextTimeTable() {
 
@@ -28,8 +67,12 @@ public class SimulatedAnnealingAlgorithm {
 
     }
 
-    public boolean swapPeriods(final short firstIndex, final short secondIndex) {
-        return true;
+    public void swapPeriods(final int firstIndex, final int secondIndex) {
+        this.nextTimeTable = this.currTimeTable.switchTwoClassSubjectInstancesAndReturn(firstIndex, secondIndex);
+    }
+
+    public void changePeriod(int index) {
+        this.nextTimeTable = this.currTimeTable.giveClassSubjectRandomPeriodAndReturn(index);
     }
 
     public boolean changeRoom(final ClassSubjectInstance classSubject) {
