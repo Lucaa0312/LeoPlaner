ebbc5b456339757c99a2358866f0d9b46e6f1b2d
Sean-Cinac <s.cinac@students.htl-leonding.ac.at>
2025-11-26 15:53:44 +0100

    new methods #49

--- leo-planer/src/main/java/at/htlleonding/leoplaner/data/Timetable.java
+++ leo-planer/src/main/java/at/htlleonding/leoplaner/data/Timetable.java
@@ -2,6 +2,7 @@ package at.htlleonding.leoplaner.data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Timetable {
     private ArrayList<ClassSubjectInstance> classSubjectInstances;
@@ -26,6 +27,15 @@ public class Timetable {
       setTotalWeeklyHours(totalHours);
     }
 
+    public boolean checkIfPeriodIsTaken(Period period) {
+        for (ClassSubjectInstance csi : classSubjectInstances) {
+            if (csi.getPeriod().getSchoolDays() == period.getSchoolDays() && csi.getPeriod().getSchoolHour() == period.getSchoolHour()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public ArrayList<ClassSubjectInstance> cloneClassSubjectInstanceList() {
         ArrayList<ClassSubjectInstance> clonedClassSubjectInstances = new ArrayList<>();
         for (ClassSubjectInstance csi : this.classSubjectInstances) {
@@ -38,13 +48,35 @@ public class Timetable {
         return new Timetable(cloneClassSubjectInstanceList());
     }
 
+    public Timetable giveClassSubjectRandomPeriodAndReturn(int index) {
+        Random random = new Random();
+        Period period;
+        do {
+            SchoolDays ranSchoolDay = SchoolDays.values()[random.nextInt(0, SchoolDays.values().length)];
+            int randSchoolHour = random.nextInt(1, 7);
+            period = new Period(ranSchoolDay, randSchoolHour);
+
+        } while (checkIfPeriodIsTaken(period));
+
+        return switchClassSubjectInstancePeriodAndReturn(index,period);
+  }
+
+    public Timetable switchClassSubjectInstancePeriodAndReturn(int index, Period newPeriod) {
+      Timetable clonedTimetable = this.cloneCurrentTimeTable();
+      ClassSubjectInstance csi = clonedTimetable.getClassSubjectInstances().get(index);
+      csi.setPeriod(newPeriod);
+      return clonedTimetable;
+  }
+
     public Timetable switchTwoClassSubjectInstancesAndReturn(int index1, int index2) {
         Timetable clonedTimetable = this.cloneCurrentTimeTable();
         ClassSubjectInstance csi1 = clonedTimetable.getClassSubjectInstances().get(index1);
         ClassSubjectInstance csi2 = clonedTimetable.getClassSubjectInstances().get(index2);
+
         Period tempPeriod = csi2.getPeriod();
         csi2.setPeriod(csi1.getPeriod());
         csi1.setPeriod(tempPeriod);
+
         return clonedTimetable;
     }
  
