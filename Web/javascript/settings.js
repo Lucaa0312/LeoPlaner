c68a459effcbea302131b309ad472639344c00f3
Julian Forster <Julfo003@gmail.com>
2025-12-09 23:28:18 +0100

    #52 Added temporary interface for teacher adding

--- Web/javascript/settings.js
+++ Web/javascript/settings.js
@@ -1,21 +1,21 @@
 function loadSubjects() {
     fetch("http://localhost:8080/api/subjects")
-    .then(response => {
-        return response.json();
-    }).then(data => {
-        console.log(data);
+        .then(response => {
+            return response.json();
+        }).then(data => {
+            console.log(data);
 
-        let subjects = ``;
-        for (let i = 0; i < data.length; i++) {
-            let subject = data[i];
-            subjects += `
-                <input type="checkbox" id="${subject.subjectName}" name="subject${subject.id}" value="${subject.subjectName}">
-                <label for="${subject.subjectName}"> ${subject.subjectName}</label>
-                <br><br>`;
-        }
+            let subjects = ``;
+            for (let i = 0; i < data.length; i++) {
+                let subject = data[i];
+                subjects += `
+                <div class="subject-box">
+                    <input type="checkbox" id="${subject.subjectName}" name="subject${subject.id}" value="${subject.subjectName}">
+                    <label for="${subject.subjectName}"> ${subject.subjectName}</label>   
+                </div>`;
+            }
 
-        let content = `
-        <div>
+            let content = `
             <form>
                 <label for="teacherName" >Teacher Name:</label>
                 <input type="text" id="teacherName" name="teacherName" required>
@@ -24,13 +24,15 @@ function loadSubjects() {
                 <input type="text" id="teacherNameSymbol" name="teacherNameSymbol" required>
                 <br><br>
                 <label for="teacherSubjects">Subjects:</label>
-                ${subjects}
-                <input type="submit" value="Add Teacher">
+                <div id="checkbox-wrapper">
+                    ${subjects}
+                </div>
+                
+                <input type="submit" id="add-teacher-btn" value="Add Teacher">
             </form>
-        </div>
         `;
-        document.getElementById("form").innerHTML = content;
-    }).catch(error => {
-        console.error('Error fetching subjects:', error);
-    });
+            document.getElementById("form").innerHTML = content;
+        }).catch(error => {
+            console.error('Error fetching subjects:', error);
+        });
 }
