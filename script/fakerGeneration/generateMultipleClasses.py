0af90680a3b5c8739d0d717ac110f3341959b158
scinac <s.cinac@students.htl-leonding.ac.at>
2026-01-30 17:34:01 +0100

    logic for faker #98

--- script/fakerGeneration/generateMultipleClasses.py
+++ script/fakerGeneration/generateMultipleClasses.py
@@ -1,3 +1,4 @@
+from typing import Required
 from faker import Faker
 import random
 
@@ -15,6 +16,19 @@ subjects = [
 ]
 days = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"]
 roomTypes = ["CLASSROOM", "EDV", "CHEM", "PHY", "SPORT", "WORKSHOP"]
+classNames = ["4CHITM", "4AHITM", "3CHITM", "3IHF", "1CHIF", "1CHITM"]
+possibleHours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+teachers = {}
+classSubjects = {}
+
+
+def createRandomClasses(count):
+    count = max(count, len(classNames))
+
+    classesSelected = random.sample(classNames, k=count)
+
+    for className in classesSelected:
+        createRandomClassSubjects(10, className)
 
 
 def createRandomRooms(count=20):
@@ -28,10 +42,67 @@ def createRandomRooms(count=20):
         numOfTypes = random.randint(1, 2)
         selectedTypes = ",".join(random.sample(roomTypes, k=numOfTypes))
 
-        # Join with semicolons
         row = f"{roomNumber};{roomName};{nameShort};{selectedTypes}"
-        print(row)
+
+
+def createRandomTeachers(count=20):
+    for i in range(count):
+        teacherName = fake.name()
+        teacherInitials = teacherName.split()[0][0] + teacherName.split()[1][0]
+
+        numOfSubjects = random.randint(1, 3)
+        teacherSubjects = []
+        teacherSubjects.append(random.sample(subjects, k=numOfSubjects))
+
+        numOfNonWorkingDays = random.randint(1, 2)
+        numOfNonWorkingHoursOnDay = random.randint(1, 8)
+        nonWorkingDays = {}
+        for _ in range(numOfNonWorkingDays):
+            nonWorkingDays[random.choice(days)] = random.sample(
+                possibleHours, k=numOfNonWorkingHoursOnDay
+            )
+
+        numOfNonPreferredDays = random.randint(1, 2)
+        numOfNonPreferredHoursOnDay = random.randint(1, 8)
+        nonPreferredDays = {}
+        for _ in range(numOfNonPreferredDays):
+            nonPreferredDays[random.choice(days)] = random.sample(
+                possibleHours, k=numOfNonPreferredHoursOnDay
+            )
+
+        teacher = {
+            "name": teacherName,
+            "initials": teacherInitials,
+            "subjects": teacherSubjects,
+            "teacherNonWorking": nonWorkingDays,
+            "teacherNonPreferred": nonPreferredDays,
+        }
+
+        teachers[i] = teacher
+
+
+def createRandomClassSubjects(classesCount, className):
+    csvHeader = "classsubject;subjectname;teachername;weeklyHours;requiresDoublePeriod;isBetterDoublePeriod;className"
+
+    for i in range(classesCount):
+        className = random.sample(classNames, k=1)
+        subject = random.sample(subjects, k=1)
+        weeklyHours = random.randint(1, 3)
+        betterDoublePeriod = fake.boolean()
+        requiredDoublePeriod = fake.boolean()
+
+        classSubject = {
+            "subject": subject,
+            "weeklyHours": weeklyHours,
+            "betterDoublePeriod": betterDoublePeriod,
+            "requiresDoublePeriod": requiredDoublePeriod,
+            "classname": className,
+        }
+
+        classSubjects[i] = classSubject
 
 
 if __name__ == "__main__":
-    createRandomRooms(5)
+    createRandomTeachers(20)
+    createRandomClasses(5)
+    print(classSubjects)
